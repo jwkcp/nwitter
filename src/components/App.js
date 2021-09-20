d2fb615ff3f500d34142b7f702a798bc1193ccf3
jwkcp <jaewoong.go@gmail.com>
2021-09-20 17:29:15 +0900

    변경된 getAuth 스펙으로 수정한 버전

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
+import authService from "fbase";
 
 function App() {
-    return <AppRouter />;
+    console.log(authService.currentUser);
+    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+    return (
+        <>
+            <AppRouter isLoggedIn={isLoggedIn} />
+            <footer>&copy; {new Date().getFullYear()}</footer>
+        </>
+    );
 }
 
 export default App;
